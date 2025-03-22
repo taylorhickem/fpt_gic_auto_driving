46e6ca24f3af3ebf6d0740e88472d9651a9e4a66
taylorhickem <taylor.hickem@gmail.com>
2025-03-22 18:44:26 +0800

    tweak prompts

--- crash/__init__.py
+++ crash/__init__.py
@@ -9,8 +9,9 @@ PROGRAM_NAME = 'Crash'
 CLI_ALIAS = 'crash'
 MESSAGES = {
     'program': {
-        'welcome': f"Welcome to {PROGRAM_NAME} CLI! Commands available: \n'{CLI_ALIAS} start'\n'{CLI_ALIAS} test'\n'{CLI_ALIAS} exit' \n ...",
-        'start': f"Welcome to {PROGRAM_NAME}! Auto driving car simulation.\n\nPlease enter the width and height of the simulation field in x y format:",
+        'welcome': f"Welcome to {PROGRAM_NAME}! Auto driving car simulation. To get started, select from any of these available commands: \n'{CLI_ALIAS} start': setup the simulation \n'{CLI_ALIAS} run': run the simulation\n'{CLI_ALIAS} test': run unit tests\n'{CLI_ALIAS} exit'\n ...",
+        'start': f"Simulation setup.\n\nPlease enter the width and height of the simulation field in x y format:",
+        'run': f"Running simulation...",
         'test': f"Running tests to verify that the program is installed correctly and works as expected ...",
         'exit': 'Exiting... Goodbye!'    
     }
